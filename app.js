bc65fdcb90ae2ae669e409ac3b78b3a4ba5af0e6
YiZheHong <yhong@brandeis.edu>
2021-06-23 22:05:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const layouts = require("express-ejs-layouts");
 
 const mongoose = require( 'mongoose' );
 //mongoose.connect( `mongodb+srv://${auth.atlasAuth.username}:${auth.atlasAuth.password}@cluster0-yjamu.mongodb.net/authdemo?retryWrites=true&w=majority`);
-mongoose.connect( 'mongodb://localhost/authDemo');//const mongoDB_URI = process.env.MONGODB_URI
+mongoose.connect( 'mongodb+srv://<username>:a838612357@152finalproject.wruf8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');//const mongoDB_URI = process.env.MONGODB_URI
 //mongoose.connect(mongoDB_URI)
 
 const db = mongoose.connection;
