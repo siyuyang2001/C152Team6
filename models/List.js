34186915ce3e11641d9a624fc14f0c625cb351a9
siyuyang2001 <siyuyang@brandeis.edu>
2021-06-24 20:18:44 -0400

    edit

--- models/List.js
+++ models/List.js
@@ -3,10 +3,10 @@ const mongoose = require( 'mongoose' );
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
-var apikeySchema = Schema( {
+var listSchema = Schema( {
   userId: ObjectId,
-  apikey: String,
-  domainName: String,
+  key: String,
+  amount: String,
 } );
 
-module.exports = mongoose.model( 'APIKey', apikeySchema );
+module.exports = mongoose.model( 'List', listSchema );
