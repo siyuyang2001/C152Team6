1f21442e1a00f3505c60e9ff144c132bf4337bfc
ruoxin <clarkyangruoxin@outlook.com>
2021-06-30 23:46:52 +0800

    yrx

--- app.js
+++ app.js
@@ -138,39 +138,39 @@ function check(inn,outt,valuee){
   if(inn=='ounce'&&outt=='pound') return 0.0625*valuee
   if(inn=='ounce'&&outt=='ounce') return valuee
 
-  if(inn=='g'&&outt=='kg') return valuee/1000
-  if(inn=='g'&&outt=='pound') return 0.0022*valuee
-  if(inn=='g'&&outt=='ounce') return 0.0353*valuee
-  if(inn=='g'&&outt=='g') return valuee
-  if(inn=='kg'&&outt=='kg') return valuee
-  if(inn=='kg'&&outt=='g') return 1000*valuee
-  if(inn=='kg'&&outt=='pound') return 2.2*valuee
-  if(inn=='kg'&&outt=='ounce') return 35.27*valuee
-  if(inn=='pound'&&outt=='kg') return 0.454*value
-  if(inn=='pound'&&outt=='pound') return valuee
-  if(inn=='pound'&&outt=='ounce') return 16*valuee
-  if(inn=='pound'&&outt=='g') return 454*valuee
-  if(inn=='ounce'&&outt=='kg') return 0.02835*valuee
-  if(inn=='ounce'&&outt=='g') return 28.35*valuee
-  if(inn=='ounce'&&outt=='pound') return 0.0625*valuee
-  if(inn=='ounce'&&outt=='ounce') return valuee
+  if(inn=='second'&&outt=='second') return valuee
+  if(inn=='second'&&outt=='minute') return valuee/60
+  if(inn=='second'&&outt=='hour') return valuee/3600
+  if(inn=='second'&&outt=='day') return valuee/3600/60
+  if(inn=='minute'&&outt=='second') return valuee*60
+  if(inn=='minute'&&outt=='minute') return valuee
+  if(inn=='minute'&&outt=='hour') return valuee/60
+  if(inn=='minute'&&outt=='day') return valuee/3600
+  if(inn=='hour'&&outt=='second') return 3600*value
+  if(inn=='hour'&&outt=='minute') return valuee*60
+  if(inn=='hour'&&outt=='hour') return valuee
+  if(inn=='hour'&&outt=='day') return valuee/60
+  if(inn=='day'&&outt=='second') return 3600*60*valuee
+  if(inn=='day'&&outt=='minute') return 3600**valuee
+  if(inn=='day'&&outt=='hour') return 60*valuee
+  if(inn=='day'&&outt=='day') return valuee
 
-  if(inn=='g'&&outt=='kg') return valuee/1000
-  if(inn=='g'&&outt=='pound') return 0.0022*valuee
-  if(inn=='g'&&outt=='ounce') return 0.0353*valuee
-  if(inn=='g'&&outt=='g') return valuee
-  if(inn=='kg'&&outt=='kg') return valuee
-  if(inn=='kg'&&outt=='g') return 1000*valuee
-  if(inn=='kg'&&outt=='pound') return 2.2*valuee
-  if(inn=='kg'&&outt=='ounce') return 35.27*valuee
-  if(inn=='pound'&&outt=='kg') return 0.454*value
-  if(inn=='pound'&&outt=='pound') return valuee
-  if(inn=='pound'&&outt=='ounce') return 16*valuee
-  if(inn=='pound'&&outt=='g') return 454*valuee
-  if(inn=='ounce'&&outt=='kg') return 0.02835*valuee
-  if(inn=='ounce'&&outt=='g') return 28.35*valuee
-  if(inn=='ounce'&&outt=='pound') return 0.0625*valuee
-  if(inn=='ounce'&&outt=='ounce') return valuee
+  if(inn=='cm'&&outt=='cm') return valuee
+  if(inn=='cm'&&outt=='m') valuee/100
+  if(inn=='cm'&&outt=='inch') return 0.394*valuee
+  if(inn=='cm'&&outt=='foot') return 0.0328valuee
+  if(inn=='m'&&outt=='cm') return valuee*100
+  if(inn=='m'&&outt=='m') return valuee
+  if(inn=='m'&&outt=='inch') return 39.4*valuee
+  if(inn=='m'&&outt=='foot') return 3.28*valuee
+  if(inn=='inch'&&outt=='cm') return 2.54*value
+  if(inn=='inch'&&outt=='m') return 0.0254valuee
+  if(inn=='inch'&&outt=='inch') return valuee
+  if(inn=='inch'&&outt=='foot') return 0.083*valuee
+  if(inn=='foot'&&outt=='cm') return 30.48*valuee
+  if(inn=='foot'&&outt=='m') return 0.3048*valuee
+  if(inn=='foot'&&outt=='inch') return 12*valuee
+  if(inn=='foot'&&outt=='foot') return valuee
 }
 
 app.get('/Sports',(req,res) => {
